15d595f39bb43346d29258f5edc2df199838dbfe
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-02 14:15:51 +0330

    step two
    complete main component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,17 +5,24 @@ const App = () => {
   const [input, setInput] = useState('')
 
   return (
-    <div>
-      <h1>UseLocalStorage</h1>
+    <div className='flex flex-col bg-gray-950 gap-4 w-screen h-screen items-center justify-center'>
+      <h1 className='text-white text-4xl font-bold'>UseLocalStorage</h1>
       <input 
       type='text'
-      value={}
-      onChange={}
+      value={input}
+      onChange={() => setInput(e.target.value)}
+      className='bg-gray-700 outline-none  focus:border border-yellow-500 shadow-md text-xl text-white rounded-md w-[1200px] px-6 py-4'
       />
       <div>
-        <button onClick={}>Set</button>
-        <button onClick={}>Get</button>
-        <button onClick={}>Remove</button>
+        <button 
+        className='bg-yellow-500 hover:bg-yellow-600 text-xl text-black px-4 py-4 rounded-md' 
+        onClick={() => setItem(input)}>Set</button>
+        <button 
+        className='bg-yellow-500 hover:bg-yellow-600 text-xl text-black px-4 py-4 rounded-md'
+        onClick={() => console.log(getItem())}>Get</button>
+        <button 
+        className='bg-yellow-500 hover:bg-yellow-600 text-xl text-black px-4 py-4 rounded-md'
+        onClick={removeItem}>Remove</button>
       </div>
     </div>
   )
