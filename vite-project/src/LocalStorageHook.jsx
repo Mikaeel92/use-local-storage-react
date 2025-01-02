e03213fcefc792d1e367a596d1e53eaf9e68933f
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-02 14:23:13 +0330

    step three
    custom hook

---
+++ vite-project/src/LocalStorageHook.jsx
@@ -0,0 +1,28 @@
+
+export const localStorageHook = (key) => {
+
+const setItem = () => {
+try {
+
+} catch (error) {
+    console.log(error)
+}
+}
+
+const getItem = () => {
+    try {
+
+} catch (error) {
+    console.log(error)
+}
+}
+
+const removeItem = () => {
+    try {
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+}
