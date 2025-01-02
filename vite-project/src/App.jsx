23129c1afc15ee7baafc6898e274a92037d07928
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-02 14:29:02 +0330

    final step & done!

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
+import { localStorageHook } from './LocalStorageHook'
 
 const App = () => {
 
   const [input, setInput] = useState('')
+
+  const {setItem, getItem, removeItem} = localStorageHook('name')
 
   return (
     <div className='flex flex-col bg-gray-950 gap-4 w-screen h-screen items-center justify-center'>
@@ -10,10 +13,10 @@ const App = () => {
       <input 
       type='text'
       value={input}
-      onChange={() => setInput(e.target.value)}
+      onChange={(e) => setInput(e.target.value)}
       className='bg-gray-700 outline-none  focus:border border-yellow-500 shadow-md text-xl text-white rounded-md w-[1200px] px-6 py-4'
       />
-      <div>
+      <div className='flex gap-4'>
         <button 
         className='bg-yellow-500 hover:bg-yellow-600 text-xl text-black px-4 py-4 rounded-md' 
         onClick={() => setItem(input)}>Set</button>
--- vite-project/src/LocalStorageHook.jsx
+++ vite-project/src/LocalStorageHook.jsx
@@ -1,28 +1,33 @@
 
 export const localStorageHook = (key) => {
-
-const setItem = () => {
-try {
-
-} catch (error) {
-    console.log(error)
-}
-}
-
-const getItem = () => {
-    try {
-
-} catch (error) {
-    console.log(error)
-}
-}
-
-const removeItem = () => {
-    try {
-
-    } catch (error) {
-        console.log(error)
+    const setItem = (value) => {
+        try {
+            window.localStorage.setItem(key, JSON.stringify(value))
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
-}
-
-}
+    
+    const getItem = () => {
+        try {
+            const item = window.localStorage.getItem(key)
+            return item ? JSON.parse(item) : undefined
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    
+    const removeItem = () => {
+        try {
+            window.localStorage.removeItem(key)
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+    
+    return {setItem, getItem, removeItem }
+    
+    }
